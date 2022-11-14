d01f1f39c728c111a3ba0d12b2c42737e787230b
Sabbir-Ksae <sabbirhossainksa3@gmail.com>
2022-11-14 06:29:32 +0300

    Practice Scanf defult Function

---
+++ Scanf/scanf.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+
+  //=================================================
+  // int num,num2;
+
+  // printf("Enter an integer number : ");
+  // scanf("%d",&num);
+
+  // printf("Enter another integer number : ");
+  // scanf("%d",&num2);
+  // printf("Number are : %d %d\n",num,num2);
+
+  //=================================================
+
+  // int num,num2;
+
+  // printf("Enter any two integer number : ");
+  // scanf("%d %d",&num,&num2);
+
+  // printf("Your Namber are : %d %d",num,num2);
+
+  //====================================================
+
+  // int num;
+  // float num2;
+
+  // printf("Enter an integer number : ");
+  // scanf("%d",&num);
+
+  // printf("Enter an float number : ");
+  // scanf("%f",&num2);
+
+  // printf("Your Integer and float number : %d %.2f",num,num2);
+
+  //======================================================
+
+  int num;
+  float num2;
+
+  printf("Enter Integer and float number : ");
+  scanf("%d %f", &num, &num2);
+
+  printf("Your Integer And Float Number Are : %d %.2f", num, num2);
+
+  return 0;
+}
