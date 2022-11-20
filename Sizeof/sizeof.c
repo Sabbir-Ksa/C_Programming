291906b775de1413da5e651435da87fe55cd4c7e
programmer-sabbir <programmersabbir@localhost.localdomain>
2022-11-19 22:37:33 -0500

    Sizeof

---
+++ Sizeof/sizeof.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int i;
+float f;
+double d;
+char c;
+
+printf("Type Of Integer = %d Bytes\n",sizeof(i));
+printf("Type Of Integer = %d Bytes\n",sizeof(f));
+printf("Type Of Integer = %d Bytes\n",sizeof(d));
+printf("Type Of Integer = %d Bytes\n",sizeof(c));
+
+
+return 0;
+}
